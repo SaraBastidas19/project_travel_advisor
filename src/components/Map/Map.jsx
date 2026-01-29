895cd890c45ec5d9b3d1467b8e6da51250263f65
Sara Bastidas <sara.bastidas@upb.edu.co>
2026-01-28 23:27:13 -0500

    Refactor Map component to use inline styles and fix MUI compatibility

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import Rating from "@mui/material/Rating";
+
 const Map = () => {
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <header> 
-        <h1>Map</h1>  
-    </header>
+    <div style={{ height: '85vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{}}
+        onChange={() => {}}
+        onChildClick={() => {}}
+      >
+      </GoogleMapReact>
+    </div>
   );
 }
 
